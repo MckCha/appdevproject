630a2fd7fa595524b6b301985b893bb55564004e
ctennison <ctennison1@csub.edu>
2022-12-06 20:53:11 -0800

    Upload and Show Video

--- appdevproj/creatorstudio.php
+++ appdevproj/creatorstudio.php
@@ -4,27 +4,67 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,
     initial-scale=1.0">
+    <link rel="stylesheet" href="project.css">
     <title>Creator Studio</title>
-    <style>
-        body {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-direction: column;
-            min-height: 100vh;
-        }
-        input {
-            font-size: 2rem;
-        }
-    </style>
 </head>
 <body>
     <?php if (isset($_GET['error'] )) { ?>
         <p><?=$_GET['error']?></p>
     <?php } ?>
-    <form action="upload.php" method="post" enctype="multipart/form-data">
-        <input type="text" name="my_video" value="Insert URL">
+    <form method="post" enctype="multipart/form-data">
+        Insert Video Link: <input type="text" name="my_video"> 
+        Subject: <input type="text" name="subject">
         <input type="submit" name="submit" value="Upload">
     </form>
+
+
+<?php session_start();
+
+function get_connection() {
+    static $connection;
+    
+    if (!isset($connection)) {
+        // Connect to the database
+        $connection = mysqli_connect('localhost', 'learnapp', 
+            'job19Fmuslk','learnapp') 
+            or die(mysqli_connect_error());
+    }
+    if ($connection === false) {
+        echo "Unable to connect to database<br/>";
+        echo mysqli_connect_error();
+    }
+  
+    return $connection;
+  }
+  
+$db = get_connection();
+
+
+if (isset($_POST['submit']) && !empty($_POST['my_video']) && !empty($_POST['subject'])) {
+    
+    $result = $db->prepare("Call UploadVid(?,?)");
+    $video_link = htmlspecialchars($_POST['my_video']);
+    $subject = htmlspecialchars($_POST['subject']);
+    $result->bind_param("ss", $subject, $video_link);
+	$result->execute();
+    //$query = $db->prepare("SELECT * lesson(lesson_subject, vid_link) VALUES(?, ?)");
+
+    //$stmt = $db->prepare("INSERT INTO lesson(lesson_subject, vid_link) VALUES(?, ?)");
+    //$stmt->bind_param("ss", $subject, $video_link);
+
+    $db->close();
+    //print_r($_POST['my_video']);
+    //echo $video_link;
+?>
+<!--<div class="video"></br>-->
+<div class="video">
+<?php
+    echo "<br></br>";
+    echo "<iframe src=$video_link height='400' width='500' ></iframe>";
+}
+?>
+</div>
+
+
 </body>
 </html>
--- appdevproj/upload.php
+++ appdevproj/upload.php
@@ -1,34 +1,70 @@
-<?php
-
-if (isset($_POST['submit']) && isset($_POST['my_video'])) {
-    $video_link = $_POST['my_video'];
-    //print_r($_POST['my_video']);
-}else{
-    header("Location: index.php");
-}
-//echo"<pre>";
-    //print_r($_FILES['my_video']);
-
-   //$video_name = $_FILES['my_video']['name'];
-   //$tmp_name = $_FILES['my_video']['tmp_name'];
-   //$error = $_FILES['my_video']['error'];
-
-   //if ($error === 0) {
-   //    $video_ex = pathinfo($video_name, PATHINFO_EXTENSION);
-
-   //    //echo $video_ex;
-   //    $video_ex_lc = strtolower($video_ex);
-
-   //    $allowed_exs = array("mp4", 'webm', 'avi', 'flv');
-
-   //    if (in_array($video_ex_lc, $allowed_exs)) {
-   //        //echo "YES";
-   //        $new_video_name = uniqid("video-", true). '.' .$video_ex_lc;
-   //        $video_upload_path = 'uploads/' .$new_video_name;
-   //        move_uploaded_file($tmp_name, $video_upload_path);
-
-   //    }else {
-   //        $em = "You cannot upload this file type";
-   //        header("Location: index.php?error=$em");
-   //    }
-   //}
+<?php session_start();
+//
+//function get_connection() {
+//    static $connection;
+//    
+//    if (!isset($connection)) {
+//        // Connect to the database
+//        $connection = mysqli_connect('localhost', 'learnapp', 
+//            'job19Fmuslk','learnapp') 
+//            or die(mysqli_connect_error());
+//    }
+//    if ($connection === false) {
+//        echo "Unable to connect to database<br/>";
+//        echo mysqli_connect_error();
+//    }
+//  
+//    return $connection;
+//  }
+//  
+//  $db = get_connection();
+//
+//
+//if (isset($_POST['submit']) && !empty($_POST['my_video']) && !empty($_POST['subject'])) {
+//    $video_link = htmlspecialchars($_POST['my_video']);
+//    $subject = htmlspecialchars($_POST['subject'])
+//
+//    $stmt = $db->prepare("INSERT INTO lesson(subject, vid_link) VALUES($subject, $video_link)");
+//    //$stmt->bind_param("ss", $subject, $video_link);
+//    $stmt->execute();
+//    $result = $stmt->get_result();
+//   
+//    if ($result !== false) {
+//        //$_SESSION['logged_in'] = true;
+//        header("Location: creatorstudio.php");
+//    }   
+//    $result->free();
+//    $db->close();
+//    //print_r($_POST['my_video']);
+//}
+//
+//    //}else{
+////    header("Location: creatorstudio.php");
+////}
+////echo"<pre>";
+//    //print_r($_FILES['my_video']);
+//
+//   //$video_name = $_FILES['my_video']['name'];
+//   //$tmp_name = $_FILES['my_video']['tmp_name'];
+//   //$error = $_FILES['my_video']['error'];
+//
+//   //if ($error === 0) {
+//   //    $video_ex = pathinfo($video_name, PATHINFO_EXTENSION);
+//
+//   //    //echo $video_ex;
+//   //    $video_ex_lc = strtolower($video_ex);
+//
+//   //    $allowed_exs = array("mp4", 'webm', 'avi', 'flv');
+//
+//   //    if (in_array($video_ex_lc, $allowed_exs)) {
+//   //        //echo "YES";
+//   //        $new_video_name = uniqid("video-", true). '.' .$video_ex_lc;
+//   //        $video_upload_path = 'uploads/' .$new_video_name;
+//   //        move_uploaded_file($tmp_name, $video_upload_path);
+//
+//   //    }else {
+//   //        $em = "You cannot upload this file type";
+//   //        header("Location: index.php?error=$em");
+//   //    }
+//   //}
+//?>
